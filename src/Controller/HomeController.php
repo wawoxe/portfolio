5b1ca13fbb9b432da61ed004b5a3f268e2afbc27
Mykyta Melnyk <wawoxe@proton.me>
2024-06-12 19:52:49 +0200

    #1 Fixed code style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * (c) Mykyta Melnyk <wawoxe@proton.me>
+ *
+ * SEE LICENSE IN ROOT FOLDER.
+ */
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -7,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
-    name: 'app_home_'
+    name: 'app_home_',
 )]
 final class HomeController extends AbstractController
 {
