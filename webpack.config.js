9dea516584069e19ded85526e32725ae4d1438ba
Mykyta Melnyk <wawoxe@proton.me>
2024-06-12 19:08:47 +0200

    #1 Added SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.ts')
+    .addEntry('base', './assets/base.ts')
+    .addStyleEntry('base_style', './assets/styles/base.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -54,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
